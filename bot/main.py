f544466f1975dcdf5a43384238fe008a6cafc690
MenegussiT <felipe.menegussi.faria@gmail.com>
2025-04-06 11:57:24 -0300

    Criação do scraping das mensagens.

--- bot/main.py
+++ bot/main.py
@@ -1,15 +1,19 @@
+import asyncio
 from database.db import Base, engine
 from database import models
-
+from telegram_utils import start_client, client
 
 def init_db():
     Base.metadata.create_all(bind=engine)
 
-
-def main():
+async def main():
     init_db()
     print("Banco de dados inicializado com sucesso!")
 
+    await start_client()  # inicia o bot e escuta mensagens
+
 
 if __name__ == "__main__":
-    main()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(start_client())
+    client.run_until_disconnected()
