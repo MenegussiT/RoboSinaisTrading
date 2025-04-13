62b278612daa6fc12a74afc3d2d6bfa63c9ba704
MenegussiT <felipe.menegussi.faria@gmail.com>
2025-04-13 10:09:39 -0300

    Criação de grupo temporário para o envio dos sinais.

--- bot/telegram_utils.py
+++ bot/telegram_utils.py
@@ -5,20 +5,19 @@ from telethon import TelegramClient, events
 from datetime import datetime
 from bot.message_parser import parse_signal_message
 
-
-
 load_dotenv()
 
 api_id = os.getenv("TELEGRAM_API_ID")
 api_hash = os.getenv("TELEGRAM_API_HASH")
 session_name = os.getenv("TELEGRAM_SESSION_NAME", "robo_sinais")
-GROUP_ID = int(os.getenv("TELEGRAM_GROUP_ID", "-1002008142020"))  # Mai Trader Free [ -1002439641464]
+# GROUP_ID = int(os.getenv("TELEGRAM_GROUP_ID", "-1002008142020"))  # Mai Trader Free [ -1002439641464]
+GROUP_ID_TEMP = int(os.getenv("TELEGRAM_GROUP_ID_TEMP"))
 
 client = TelegramClient(session_name, api_id, api_hash)
 
 @client.on(events.NewMessage)
 async def handle_new_message(event):
-    if event.chat_id != GROUP_ID:
+    if event.chat_id != GROUP_ID_TEMP:
         return  # Ignora outras conversas
 
     mensagem = event.message.message
@@ -30,6 +29,8 @@ async def handle_new_message(event):
     # Aqui você pode acionar funções específicas para operar o sinal
     if dados["ativo"] and dados["entrada"]:
         print(f"✅ Sinal válido detectado para {dados['ativo']} às {dados['entrada'].time()}")
+    else:
+        print("⚠️ Mensagem fora do padrão. Ignorada.")
 
 async def start_client():
     print("✅ Cliente conectado e rodando!")
